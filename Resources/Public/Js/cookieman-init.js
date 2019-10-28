e666a7ed5c428735d146c29d30f6c4b4e6389598
Jonas Eberle <jonas.eberle@d-mind.de>
2019-10-28 18:16:02 +0100

    refactor initialisation and JS theme integration

---
+++ Resources/Public/Js/cookieman-init.js
@@ -0,0 +1,5 @@
+// show consent popup (once) if we find a data-cookieman-showonce="1" tag somewhere (used e.g. to suppress on specific 
+// pages)
+if (null !== document.querySelector('[data-cookieman-showonce="1"]')) {
+    cookieman.showOnce()
+}
--- Resources/Public/Themes/bootstrap3-banner/cookieman-theme.js
+++ Resources/Public/Themes/bootstrap3-banner/cookieman-theme.js
@@ -1,24 +1,18 @@
 // requires: cookieman.js, Bootstrap-JS, jQuery
 cookieman.theme = (function () {
     "use strict";
-    var $modal = jQuery('#cookieman-modal'),
-        showBackdrop = true
+    var showBackdrop = true
 
     cookieman.show = function () {
-        $modal.modal({show: true, backdrop: showBackdrop})
+        jQuery(function () {
+            var $modal = jQuery('#cookieman-modal')
+            $modal.modal({show: true, backdrop: showBackdrop})
+        })
     }
     cookieman.hide = function () {
-        $modal.modal('hide')
+        jQuery(function () {
+            var $modal = jQuery('#cookieman-modal')
+            $modal.modal('hide')
+        })
     }
-
-    // example if you need to initialize
-    // return {
-    //     init: function () {
-    //     }
-    // }
 }())
-
-// example if you need to initialize
-// jQuery(function () { // if you need jQuery
-//     cookieman.theme.init()
-// })
