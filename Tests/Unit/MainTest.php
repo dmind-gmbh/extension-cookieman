fc2818395e71db4e334af405c987c6c1d3b23e8a
Jonas Eberle <jonas.eberle@d-mind.de>
2019-11-06 22:17:18 +0100

    add unit tests skeleton

---
+++ Tests/Unit/MainTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+namespace Dmind\Cookieman\Tests\Unit;
+
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class MainTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function soTrue() {
+
+    }
+}
