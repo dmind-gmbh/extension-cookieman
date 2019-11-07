303e2557e8bf31d65736960d0c0510a32f862856
Jonas Eberle <jonas.eberle@d-mind.de>
2019-11-07 14:27:57 +0100

    setup TypoScript in cookieman_test

--- Build/cookieman_test/Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-// static TypoScript
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'cookieman_test',
-    'Configuration/TypoScript',
-    'Cookieman Test'
-);
