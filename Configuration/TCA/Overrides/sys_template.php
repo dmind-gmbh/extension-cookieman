d95f80ed4eae360e1953bc13570d9da834f98af6
Jonas Eberle <jonas.eberle@d-mind.de>
2019-11-07 14:28:31 +0100

    fix PHP

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,9 +1,19 @@
 <?php
+
+/*
+ * This file is part of the package dmind/cookieman.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 defined('TYPO3_MODE') || die();
 
 // static TypoScript
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'cookieman',
-    'Configuration/TypoScript',
-    'Cookieman'
-);
+(static function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        'cookieman',
+        'Configuration/TypoScript',
+        'Cookieman'
+    );
+})();
--- Tests/Unit/MainTest.php
+++ Tests/Unit/MainTest.php
@@ -1,5 +1,13 @@
 <?php
 declare(strict_types = 1);
+
+/*
+ * This file is part of the package dmind/cookieman.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace Dmind\Cookieman\Tests\Unit;
 
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
@@ -9,7 +17,7 @@ class MainTest extends UnitTestCase
     /**
      * @test
      */
-    public function soTrue() {
-
+    public function soTrue()
+    {
     }
 }
