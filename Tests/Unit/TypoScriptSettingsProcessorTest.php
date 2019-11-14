1bcf9b9046e809cc38e064c827532a0c783fd930
Jonas Eberle <jonas.eberle@d-mind.de>
2019-11-14 19:59:02 +0100

    add DataProcessor test
    (cherry picked from commit 0108fbd1e57cee6c0a909ad54de6a404c0194fb3)

---
+++ Tests/Unit/TypoScriptSettingsProcessorTest.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the package dmind/cookieman.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Dmind\Cookieman\Tests\Unit;
+
+use Dmind\Cookieman\DataProcessing\TypoScriptSettingsProcessor;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class TypoScriptSettingsProcessorTest extends UnitTestCase
+{
+    /**
+     * @var ContentObjectRenderer|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $contentObjectRenderer;
+
+    /**
+     * Sets up this testcase
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->contentObjectRenderer = $this->getMockBuilder(ContentObjectRenderer::class)
+            ->getMock();
+    }
+
+    /**
+     * @test
+     */
+    public function returnsSettings()
+    {
+        $configurationManager = $this->getMockBuilder(ConfigurationManagerInterface::class)->getMockForAbstractClass();
+        $configurationManager->expects(self::once())->method('getConfiguration')->willReturn(
+            [
+                'settings' => [
+                    'groups' => [
+                        'mandatory' => [
+                            'preselected' => 1,
+                        ],
+                    ],
+                ]
+            ]
+        );
+
+        $subject = $this->getMockBuilder(TypoScriptSettingsProcessor::class)
+            ->onlyMethods(['getConfigurationManager'])
+            ->setMockClassName('ConfigurationManagerInterface')->getMock();
+        $subject->expects(self::once())->method('getConfigurationManager')->willReturn($configurationManager);
+
+        $result = $subject->process(
+            $this->contentObjectRenderer,
+            [],
+            [],
+            ['data' => []]
+        );
+
+        self::assertEquals(
+            [
+                'data' => [],
+                'settings' => [
+                    'settings' => [
+                        'groups' => [
+                            'mandatory' => [
+                                'preselected' => 1,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            $result
+        );
+    }
+}
