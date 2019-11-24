fab9bc18c1bc034421283c47fba16294c9c44091
Jonas Eberle <jonas.eberle@d-mind.de>
2019-11-24 14:45:45 +0100

    closes #35 add API call onScriptLoaded() to register script onload callbacks. (#28)

--- Resources/Public/Js/cookieman.js
+++ Resources/Public/Js/cookieman.js
@@ -6,11 +6,13 @@ var cookieman = (function () {
         cookieLifetimeDays = 365,
         form = document.querySelector('[data-cookieman-form]'),
         settingsEl = document.querySelector('[data-cookieman-settings]'),
+        eventsEl = settingsEl,
         settings = JSON.parse(settingsEl.dataset.cookiemanSettings),
         checkboxes = form.querySelectorAll('[type=checkbox][name]'),
         saveButtons = document.querySelectorAll('[data-cookieman-save]'),
         acceptAllButtons = document.querySelectorAll('[data-cookieman-accept-all]'),
-        injectedTrackingObjects = []
+        injectedTrackingObjects = [],
+        loadedTrackingObjectScripts = {}
 
     function saveSelections() {
         var consented = []
@@ -97,11 +99,11 @@ var cookieman = (function () {
 
     /**
      * inject the HTML for a given tracking object
-     * @param trackingObjectId string e.g. 'Matomo'
+     * @param trackingObjectKey string e.g. 'Matomo'
      * @param trackingObjectSettings array (e.g. the array plugin.tx_cookieman.settings.trackingObjects.Matomo
      * from TypoScript)
      */
-    function injectTrackingObject(trackingObjectId, trackingObjectSettings) {
+    function injectTrackingObject(trackingObjectKey, trackingObjectSettings) {
         if (typeof trackingObjectSettings.inject !== "undefined") {
             // <script>s inserted via innerHTML won't be executed
             // https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
@@ -110,25 +112,52 @@ var cookieman = (function () {
             var pseudo = document.createElement('div'),
                 _script
             pseudo.innerHTML = trackingObjectSettings.inject
-
             // ... insert each node ...
-            for (var _i = 0; _i < pseudo.children.length; _i++) {
-                var node = pseudo.children[_i]
+            var iScript = 0
+            for (var iChild = 0; iChild < pseudo.children.length; iChild++) {
+                var node = pseudo.children[iChild]
                 // ... and give special treatment to <script>s
                 if (node.tagName === 'SCRIPT') {
                     _script = document.createElement('script')
                     _script.textContent = node.textContent
                     for (var _iAttr = 0; _iAttr < node.attributes.length; _iAttr++) {
-                        var _attr = node.attributes[_iAttr];
-                        _script.setAttribute(_attr.name, _attr.value);
+                        var _attr = node.attributes[_iAttr]
+                        _script.setAttribute(_attr.name, _attr.value)
                     }
+                    _script.addEventListener(
+                        'load',
+                        (
+                            function (_script, iScript, trackingObjectKey, trackingObjectSettings) {
+                                return function (ev) {
+                                    if (typeof loadedTrackingObjectScripts[trackingObjectKey] === 'undefined') {
+                                        loadedTrackingObjectScripts[trackingObjectKey] = []
+                                    }
+                                    loadedTrackingObjectScripts[trackingObjectKey].push(iScript)
+                                    emit(
+                                        'scriptLoaded',
+                                        {
+                                            detail: {
+                                                trackingObjectKey: trackingObjectKey,
+                                                trackingObjectSettings: trackingObjectSettings,
+                                                scriptId: iScript,
+                                                node: _script
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )(_script, iScript++, trackingObjectKey, trackingObjectSettings)
+                    )
                     node = _script
+                } else {
+                    // we will be removing this child
+                    iChild--
                 }
                 document.body.appendChild(node)
             }
 
             // keep track what we injected
-            injectedTrackingObjects.push(trackingObjectId)
+            injectedTrackingObjects.push(trackingObjectKey)
         }
     }
 
@@ -140,12 +169,34 @@ var cookieman = (function () {
         for (var _i = 0; _i < consenteds.length; _i++) {
             var aGroup = settings.groups[consenteds[_i]]
             for (var _j = 0; _j < aGroup.trackingObjects.length; _j++) {
-                var trackingObjectId = aGroup.trackingObjects[_j]
-                if (injectedTrackingObjects.indexOf(trackingObjectId) === -1) {
-                    injectTrackingObject(trackingObjectId, settings.trackingObjects[trackingObjectId])
+                var trackingObjectKey = aGroup.trackingObjects[_j]
+                if (injectedTrackingObjects.indexOf(trackingObjectKey) === -1) {
+                    injectTrackingObject(trackingObjectKey, settings.trackingObjects[trackingObjectKey])
                 }
             }
         }
+    }
+
+    // CustomEvents https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+    // polyfill for IE9+
+    function polyfillCustomEvent() {
+        if (typeof window.CustomEvent !== "function") {
+            window.CustomEvent = function (typeArg, customEventInit) {
+                customEventInit = customEventInit || {bubbles: false, cancelable: false, detail: undefined}
+                var event = document.createEvent('CustomEvent')
+                event.initCustomEvent(typeArg, customEventInit.bubbles, customEventInit.cancelable, customEventInit.detail)
+                return event
+            }
+            window.CustomEvent.prototype = window.Event.prototype
+        }
+    }
+
+    function emit(typeArg, customEventInit) {
+        polyfillCustomEvent()
+
+        eventsEl.dispatchEvent(
+            new CustomEvent(typeArg, customEventInit)
+        )
     }
 
     function init() {
@@ -174,26 +225,73 @@ var cookieman = (function () {
     init()
 
     return {
+        /**
+         * @api
+         */
         show: function () {
             console.error('Your theme should implement function cookieman.show()')
         },
+        /**
+         * @api
+         */
         hide: function () {
             console.error('Your theme should implement function cookieman.hide()')
         },
+        /**
+         * @api
+         */
         showOnce: function () {
             if (typeof Cookies.get(cookieName) === 'undefined') {
                 cookieman.show()
             }
         },
-        hasConsented: function (selection) {
-            var consented = consentedSelections()
+        /**
+         * @api
+         * @param {string} groupKey
+         * @returns {boolean}
+         */
+        hasConsented: function (groupKey) {
+            var consented = consentedSelectionsRespectDnt()
             for (var i = 0; i < consented.length; i++) {
-                if (consented[i] === selection) {
+                if (consented[i] === groupKey) {
                     return true
                 }
             }
             return false
         },
-        consenteds: consentedSelectionsRespectDnt
+        /**
+         * @api
+         */
+        consenteds: consentedSelectionsRespectDnt,
+        /**
+         * @api
+         * @param {string} trackingObjectKey
+         * @param {number} scriptId
+         * @param {function} callback
+         */
+        onScriptLoaded: function (trackingObjectKey, scriptId, callback) {
+            if (typeof loadedTrackingObjectScripts[trackingObjectKey] === 'undefined') {
+                loadedTrackingObjectScripts[trackingObjectKey] = []
+            }
+
+            // not loaded yet
+            if (loadedTrackingObjectScripts[trackingObjectKey].indexOf(scriptId) === -1) {
+                // attach ourselves to the "scriptLoaded" event
+                eventsEl.addEventListener(
+                    'scriptLoaded',
+                    function(ev) {
+                        if (ev.detail.trackingObjectKey === trackingObjectKey && ev.detail.scriptId === scriptId) {
+                            callback(ev.detail.trackingObjectKey, ev.detail.scriptId)
+                        }
+                    }
+                )
+            } else { // already loaded
+                callback(trackingObjectKey, scriptId)
+            }
+        },
+        /**
+         * not part of the API
+         */
+        eventsEl: eventsEl
     }
 }())
