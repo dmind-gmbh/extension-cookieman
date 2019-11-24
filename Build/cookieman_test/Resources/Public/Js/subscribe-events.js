c55ee8f9cb918ee3d1cedde5e592f3292a451a65
Jonas Eberle <jonas.eberle@d-mind.de>
2019-11-24 14:45:45 +0100

    closes #35 add API call onScriptLoaded() to register script onload callbacks. (#28)

---
+++ Build/cookieman_test/Resources/Public/Js/subscribe-events.js
@@ -0,0 +1,39 @@
+// using cookieman API
+console.log('cookieman_test: using onScriptLoaded() API')
+cookieman.onScriptLoaded(
+    'Crowdin',
+    0,
+    function (trackingObjectKey, scriptId) {
+        console.log('cookieman_test: callback onScriptLoaded() API', trackingObjectKey, scriptId)
+    }
+)
+cookieman.onScriptLoaded(
+    'Crowdin',
+    1,
+    function (trackingObjectKey, scriptId) {
+        console.log('cookieman_test: callback onScriptLoaded() API', trackingObjectKey, scriptId)
+    }
+)
+
+// subscribing to our CustomEvent "scriptLoaded" directly
+// console.log('cookieman_test: subscribing to "scriptLoaded" directly')
+// cookieman.eventsEl.addEventListener(
+//     'scriptLoaded',
+//     function (ev) {
+//         console.log('cookieman_test: scripLoaded event caught: ', ev, ev.detail)
+//     }
+// )
+
+setTimeout(
+    function () {
+        console.log('cookieman_test: using onScriptLoaded() API (after 2s)')
+        cookieman.onScriptLoaded(
+            'Crowdin',
+            1,
+            function (trackingObjectKey, scriptId) {
+                console.log('cookieman_test: callback onScriptLoaded() API (after 2s)', trackingObjectKey, scriptId)
+            }
+        )
+    },
+    2000
+)
