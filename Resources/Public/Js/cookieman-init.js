18cad87324fb13680a644d12313ee3fc55ee5757
Jonas Eberle <flightvision@googlemail.com>
2020-06-12 15:59:53 +0200

    Inhibit init() to get out of the main rendering thread and prevent early page reflow #138

--- Resources/Public/Js/cookieman-init.js
+++ Resources/Public/Js/cookieman-init.js
@@ -1,6 +1,11 @@
 // show consent popup (once) if we find a data-cookieman-showonce="1" tag somewhere (used e.g. to suppress on specific
 // pages)
 /** global: cookieman */
+
 if (null !== document.querySelector('[data-cookieman-showonce="1"]')) {
-    cookieman.showOnce()
+    // wait a bit in order to a) be executed out of the main rendering thread b) give user first impression of page
+    setTimeout(
+        cookieman.showOnce,
+        2000
+    )
 }
