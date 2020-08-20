3bdad16a9a5c223eb7a3cedde8fb7e5b6b0c4ec8
Jonas Eberle <flightvision@googlemail.com>
2020-08-20 13:32:10 +0200

    branch off 10lts

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.5.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.4.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
