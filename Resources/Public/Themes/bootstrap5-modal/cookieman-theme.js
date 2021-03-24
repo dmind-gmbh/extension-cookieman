be8ac299372305030a157d92a0a5d0c7dcdd8b05
Jonas Eberle <flightvision@googlemail.com>
2021-03-24 21:02:23 +0100

    [TASK] upgrade npm packages

--- Resources/Public/Themes/bootstrap5-modal/cookieman-theme.js
+++ Resources/Public/Themes/bootstrap5-modal/cookieman-theme.js
@@ -1,9 +1,29 @@
-var cookieModal = new bootstrap.Modal(document.getElementById('cookieman-modal'), { backdrop: 'static', keyboard: false });
+// requires: cookieman.js, Bootstrap-JS
+/** global: cookieman */
+cookieman.theme = (function () {
+  "use strict";
+  var showBackdrop = true,
+    showSettingsBtn = document.querySelector('[aria-controls="cookieman-settings"]'),
+    modal = null
 
-cookieman.show = function() {
-cookieModal.show();
-};
+  // show "save" after opening settings
+  if (showSettingsBtn) {
+    showSettingsBtn.addEventListener('click', function (ev) {
+      var saveBtn = document.querySelector('[data-cookieman-save]')
+      saveBtn && saveBtn.show()
+    })
+  }
 
-cookieman.hide = function() {
-cookieModal.hide();
-};
+  cookieman.show = function () {
+    modal = new bootstrap.Modal(
+      document.getElementById('cookieman-modal'),
+      {
+        backdrop: showBackdrop
+      }
+    )
+    modal.show()
+  }
+  cookieman.hide = function () {
+    modal && modal.hide()
+  }
+}())
