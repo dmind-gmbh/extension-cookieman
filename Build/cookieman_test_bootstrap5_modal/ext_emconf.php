4cd90f0e0e9ebb9ea3ff82e08d51f34ef65ccec9
Jonas Eberle <flightvision@googlemail.com>
2021-03-24 19:13:24 +0100

    [TASK] add test setup for bootstrap5-modal

---
+++ Build/cookieman_test_bootstrap5_modal/ext_emconf.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of the package dmind/cookieman.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'cookieman test environment (Theme bootstrap5-modal)',
+    'description' => '',
+];
