37054e3abdd5564ce1aea5d4f17f1f8962784b4e
bucheggerOnline <32962999+bucheggerOnline@users.noreply.github.com>
2021-03-24 19:54:05 +0100

    [FEATURE] add new theme "bootstrap5-modal" (#207)

---
+++ Resources/Public/Themes/bootstrap5-modal/cookieman-theme.js
@@ -0,0 +1,9 @@
+var cookieModal = new bootstrap.Modal(document.getElementById('cookieman-modal'), { backdrop: 'static', keyboard: false });
+
+cookieman.show = function() {
+cookieModal.show();
+};
+
+cookieman.hide = function() {
+cookieModal.hide();
+};
