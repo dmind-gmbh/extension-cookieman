31a6fe1ea907e37ed80b610373a4d5b78afb0067
Jonas Eberle <flightvision@googlemail.com>
2021-07-12 11:41:19 +0200

    [TASK] use WebDriver's clickWithLeftButton() since it includes moveMouseOver()
    (cherry picked from commit e08da181170ea2c4ab96b14fe14bf2e28b321f15)

--- Tests/Acceptance/Frontend/PopupInteractionsCest.php
+++ Tests/Acceptance/Frontend/PopupInteractionsCest.php
@@ -83,7 +83,7 @@ class PopupInteractionsCest
         $I->amOnPage(self::PATH_root);
         $I->waitForJS('return typeof cookieman === "object"', 10);
         $I->waitForElementVisible(self::SELECTOR_modal);
-        $I->click(['css' => self::SELECTOR_btnSaveNone]);
+        $I->clickWithLeftButton(['css' => self::SELECTOR_btnSaveNone]);
         $I->waitForElementNotVisible(self::SELECTOR_modal);
         $I->seeCookie(self::COOKIENAME);
         $I->assertEquals(
@@ -101,7 +101,7 @@ class PopupInteractionsCest
         $I->amOnPage(self::PATH_root);
         $I->waitForJS('return typeof cookieman === "object"', 10);
         $I->waitForElementVisible(self::SELECTOR_modal);
-        $I->click(self::LOCATOR_settings);
+        $I->clickWithLeftButton(self::LOCATOR_settings);
         $I->scrollIntoView(self::SELECTOR_btnSaveAll);
         $I->clickWithLeftButton(['css' => self::SELECTOR_btnSaveAll]);
         $I->waitForElementNotVisible(self::SELECTOR_modal);
@@ -137,7 +137,7 @@ class PopupInteractionsCest
         $I->amOnPage(self::PATH_root);
         $I->waitForJS('return typeof cookieman === "object"', 10);
         $I->waitForElementVisible(self::SELECTOR_modal, self::WAITFOR_timeout);
-        $I->tryToClick(self::LOCATOR_settings);
+        $I->tryToClickWithLeftButton(self::LOCATOR_settings);
         $I->waitForElementVisible(Locator::contains('*', self::GROUP_title2nd), self::WAITFOR_timeout);
         $I->tryToClickWithLeftButton(Locator::contains('*', self::GROUP_title2nd));
         $I->waitForElementVisible(
@@ -150,7 +150,7 @@ class PopupInteractionsCest
         }
         $I->seeCheckboxIsChecked('[name=' . self::GROUP_key2nd . ']');
         $I->scrollIntoView(self::SELECTOR_btnSaveNotSaveAll);
-        $I->click(['css' => self::SELECTOR_btnSaveNotSaveAll]);
+        $I->clickWithLeftButton(['css' => self::SELECTOR_btnSaveNotSaveAll]);
         $I->waitForElementNotVisible(self::SELECTOR_modal);
         $I->seeCookie(self::COOKIENAME);
         $I->assertEquals(
