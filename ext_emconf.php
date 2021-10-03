b6ee558860b41d2c1f0ddd4ed314a2541852cde0
Jonas Eberle <flightvision@googlemail.com>
2021-10-03 23:18:13 +0200

    [TASK] Adapt package description

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Cookieman',
-    'description' => 'A GDPR tracking consent popup. It asks for approval to include tracking objects (cookies, images or any HTML) and includes the objects when consented. It also removes cookies after the consent has been revoked. Notice: Supports TYPO3v8-v*, but each unique version only supports one!',
+    'description' => 'Provides a user consent popup. It asks for approval to include tracking objects (cookies, images or any HTML) and includes the objects when consented. It also removes cookies after the consent has been revoked.',
     'category' => 'fe',
     'author' => 'Jonas Eberle',
     'author_email' => 'jonas.eberle@d-mind.de',
