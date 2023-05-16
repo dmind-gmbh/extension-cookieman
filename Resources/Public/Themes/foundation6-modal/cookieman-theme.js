615f3458abd2656152d8a465d9dfb06f87b33d90
Karsten Nowak <nowak@undkonsorten.com>
2023-05-16 16:57:32 +0200

    [TASK] Get foundation 6 theme running
    Related: #23031

--- Resources/Public/Themes/foundation6-modal/cookieman-theme.js
+++ Resources/Public/Themes/foundation6-modal/cookieman-theme.js
@@ -1,9 +1,13 @@
-// requires: cookieman.js, Bootstrap-JS
+// requires: cookieman.js,
+// Foundation Reveal JS
+// Foundation Accordion JS
+// Foundation Motion-UI JS
+// jQuery
+// maybe some more default JS libraries from Foundation
 /** global: cookieman */
 (function () {
   "use strict";
-  var showBackdrop = true,
-    showSettingsBtn = document.querySelector('[aria-controls="cookieman-settings"]'),
+  var showSettingsBtn = document.querySelector('[aria-controls="cookieman-settings"]'),
     modal = null
 
   // show "save" after opening settings
@@ -15,19 +19,22 @@
       if (saveBtn) {
         saveBtn.hidden = false
       }
-    })
+        $('#cookieman-settings').foundation('toggle',$('.accordion-content-settings'));
+    });
   }
 
   cookieman.show = function () {
-    modal = new bootstrap.Modal(
-      document.getElementById('cookieman-modal'),
-      {
-        backdrop: showBackdrop
-      }
-    )
-    modal.show()
+    modal = new Foundation.Reveal(
+        $('#cookieman-modal'),
+        {
+            // options: https://get.foundation/sites/docs/reveal.html#js-options
+            'closeOnClick': false,
+            'animationIn': 'fade-in',
+            'animationOut': 'fade-out'
+        });
+      modal.open();
   }
   cookieman.hide = function () {
-    modal && modal.hide()
+    modal && modal.close()
   }
 })()
