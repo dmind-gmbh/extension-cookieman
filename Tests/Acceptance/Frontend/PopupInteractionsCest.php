87143bdcbaa419264ed33698d27b98d21cd1b737
Jonas Eberle <flightvision@googlemail.com>
2024-03-06 19:10:57 +0100

    chore(tests): refactor acceptance tests

--- Tests/Acceptance/Frontend/PopupInteractionsCest.php
+++ Tests/Acceptance/Frontend/PopupInteractionsCest.php
@@ -11,21 +11,9 @@ declare(strict_types=1);
 
 namespace Dmind\Cookieman\Tests\Acceptance\Frontend;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 use Codeception\Util\Locator;
 use Dmind\Cookieman\Tests\Acceptance\Support\AcceptanceTester;
+use Dmind\Cookieman\Tests\Acceptance\Support\Constants;
 use Exception;
 
 /**
@@ -33,62 +21,22 @@ use Exception;
  */
 class PopupInteractionsCest
 {
-    const PATH_root = '/';
-    const PATH_imprint = '/imprint';
-    const PATH_dataProtectionDeclaration = '/privacy';
-
-    const MODAL_titleEn = 'About Cookies';
-    const MODAL_textEn = 'This website uses cookies.';
-
-    const SELECTOR_modal = '#cookieman-modal';
-    const SELECTOR_btnSaveNotSaveAll = '[data-cookieman-save]:not([data-cookieman-accept-all]):not([data-cookieman-accept-none])';
-    const SELECTOR_btnSaveNone = '[data-cookieman-save][data-cookieman-accept-none]';
-    const SELECTOR_btnSaveAll = '[data-cookieman-save][data-cookieman-accept-all]';
-    const LOCATOR_settings = ['xpath' => '//*[self::button or self::a][contains(., "Settings")]'];
-    const BUTTON_titleSave = 'Save';
-
-    const COOKIENAME = 'CookieConsent';
-    const COOKIE_separator = '|';
-
-    const JS_showCookieman = 'cookieman.show()';
-    const JS_showOnceCookieman = 'cookieman.showOnce()';
-    const JS_hideCookieman = 'cookieman.hide()';
-    const JS_onScriptLoaded = "
-            cookieman.onScriptLoaded(
-                arguments[0],
-                arguments[1],
-                function (trackingObjectKey, scriptId) {
-                    document.body.append(arguments[0] + ':' + arguments[1] + ' loaded; ')
-                }
-            );
-        ";
-
-    const GROUP_keyMandatory = 'mandatory';
-
-    const GROUP_key2nd = 'marketing';
-    const GROUP_title2nd = 'Marketing';
-    const COOKIE_titleIn2ndGroup = '_gat';
-
-    const GROUP_keyTestgroup = 'testgroup';
-    const TRACKINGOBJECT_inTestgroupWith2Scripts = 'Crowdin';
-
-    const WAITFOR_timeout = 5;
-
     /**
      * @param AcceptanceTester $I
      * @throws Exception
      */
     public function save(AcceptanceTester $I)
     {
-        $I->amOnPage(self::PATH_root);
+        $I->amOnPage(Constants::PATH_root);
         $I->waitForJS('return typeof cookieman === "object"', 10);
-        $I->waitForElementVisible(self::SELECTOR_modal);
-        $I->clickWithLeftButton(['css' => self::SELECTOR_btnSaveNone]);
-        $I->waitForElementNotVisible(self::SELECTOR_modal);
-        $I->seeCookie(self::COOKIENAME);
+        $I->waitForElementVisible(Constants::SELECTOR_modal);
+        $I->waitForElementClickable(Constants::SELECTOR_btnSaveNone);
+        $I->clickWithLeftButton(['css' => Constants::SELECTOR_btnSaveNone]);
+        $I->waitForElementNotVisible(Constants::SELECTOR_modal);
+        $I->seeCookie(Constants::COOKIENAME);
         $I->assertEquals(
-            self::GROUP_keyMandatory,
-            $I->grabCookie(self::COOKIENAME, ['path' => self::PATH_root])
+            Constants::GROUP_keyMandatory,
+            $I->grabCookie(Constants::COOKIENAME, ['path' => Constants::PATH_root])
         );
     }
 
@@ -98,32 +46,29 @@ class PopupInteractionsCest
      */
     public function saveAll(AcceptanceTester $I)
     {
-        $I->amOnPage(self::PATH_root);
+        $I->amOnPage(Constants::PATH_root);
         $I->waitForJS('return typeof cookieman === "object"', 10);
-        $I->waitForElementVisible(self::SELECTOR_modal);
-        $I->clickWithLeftButton(self::LOCATOR_settings);
-        $I->scrollIntoView(self::SELECTOR_btnSaveAll);
-        $I->clickWithLeftButton(['css' => self::SELECTOR_btnSaveAll]);
-        $I->waitForElementNotVisible(self::SELECTOR_modal);
-        $I->seeCookie(self::COOKIENAME);
+        $I->waitForElementVisible(Constants::SELECTOR_modal);
+        $I->waitForElementClickable(Constants::LOCATOR_settings);
+        $I->clickWithLeftButton(Constants::LOCATOR_settings);
+        $I->scrollIntoView(Constants::SELECTOR_btnSaveAll);
+        $I->waitForElementClickable(Constants::SELECTOR_btnSaveAll);
+        $I->clickWithLeftButton(['css' => Constants::SELECTOR_btnSaveAll]);
+        $I->waitForElementNotVisible(Constants::SELECTOR_modal);
+        $I->seeCookie(Constants::COOKIENAME);
         $I->assertStringStartsWith(
-            $this->cookieValueForGroups([self::GROUP_keyMandatory, self::GROUP_key2nd]),
-            $I->grabCookie(self::COOKIENAME, ['path' => self::PATH_root])
+            $this->cookieValueForGroups([Constants::GROUP_keyMandatory, Constants::GROUP_key2nd]),
+            $I->grabCookie(Constants::COOKIENAME, ['path' => Constants::PATH_root])
         );
     }
 
     /**
-     * @param AcceptanceTester $I
-     * @throws Exception
+     * @param array $groupKeys
+     * @return string
      */
-    public function notShownOnImprint(AcceptanceTester $I)
+    protected function cookieValueForGroups(array $groupKeys)
     {
-        $I->amOnPage(self::PATH_imprint);
-        $I->waitForJS('return typeof cookieman === "object"', 10);
-        $I->wait(self::WAITFOR_timeout);
-        $I->dontSeeElement(self::SELECTOR_modal);
-        $I->executeJS(self::JS_showCookieman);
-        $I->waitForElementVisible(self::SELECTOR_modal, self::WAITFOR_timeout);
+        return implode(Constants::COOKIE_separator, $groupKeys);
     }
 
     /**
@@ -134,28 +79,31 @@ class PopupInteractionsCest
     public function selectGroupAndSaveMobile(AcceptanceTester $I)
     {
         $I->resizeWindow(480, 800);
-        $I->amOnPage(self::PATH_root);
+        $I->amOnPage(Constants::PATH_root);
         $I->waitForJS('return typeof cookieman === "object"', 10);
-        $I->waitForElementVisible(self::SELECTOR_modal, self::WAITFOR_timeout);
-        $I->tryToClickWithLeftButton(self::LOCATOR_settings);
-        $I->waitForElementVisible(Locator::contains('*', self::GROUP_title2nd), self::WAITFOR_timeout);
-        $I->tryToClickWithLeftButton(Locator::contains('*', self::GROUP_title2nd));
+        $I->waitForElementVisible(Constants::SELECTOR_modal, Constants::WAITFOR_timeout);
+        $I->waitForElementClickable(Constants::LOCATOR_settings);
+        $I->clickWithLeftButton(Constants::LOCATOR_settings);
+        $I->waitForElementVisible(Locator::contains('*', Constants::GROUP_title2nd), Constants::WAITFOR_timeout);
+        $I->waitForElementClickable(Locator::contains('*', Constants::GROUP_title2nd));
+        $I->clickWithLeftButton(Locator::contains('*', Constants::GROUP_title2nd));
         $I->waitForElementVisible(
-            Locator::contains('*', self::COOKIE_titleIn2ndGroup),
-            self::WAITFOR_timeout
+            Locator::contains('*', Constants::COOKIE_titleIn2ndGroup),
+            Constants::WAITFOR_timeout
         ); // a single row in the table
-        $I->scrollIntoView('[name=' . self::GROUP_key2nd . ']');
-        if (!$I->tryToCheckOption('[name=' . self::GROUP_key2nd . ']')) { // theme: *-modal
-            $I->executeJS('$("[name=' . self::GROUP_key2nd . ']").click()'); // theme: bootstrap3-banner
+        $I->scrollIntoView('[name=' . Constants::GROUP_key2nd . ']');
+        if (!$I->tryToCheckOption('[name=' . Constants::GROUP_key2nd . ']')) { // theme: *-modal
+            $I->executeJS('$("[name=' . Constants::GROUP_key2nd . ']").click()'); // theme: bootstrap3-banner
         }
-        $I->seeCheckboxIsChecked('[name=' . self::GROUP_key2nd . ']');
-        $I->scrollIntoView(self::SELECTOR_btnSaveNotSaveAll);
-        $I->clickWithLeftButton(['css' => self::SELECTOR_btnSaveNotSaveAll]);
-        $I->waitForElementNotVisible(self::SELECTOR_modal);
-        $I->seeCookie(self::COOKIENAME);
+        $I->seeCheckboxIsChecked('[name=' . Constants::GROUP_key2nd . ']');
+        $I->scrollIntoView(Constants::SELECTOR_btnSaveNotSaveAll);
+        $I->waitForElementClickable(Constants::SELECTOR_btnSaveNotSaveAll);
+        $I->clickWithLeftButton(['css' => Constants::SELECTOR_btnSaveNotSaveAll]);
+        $I->waitForElementNotVisible(Constants::SELECTOR_modal);
+        $I->seeCookie(Constants::COOKIENAME);
         $I->assertEquals(
-            $this->cookieValueForGroups([self::GROUP_keyMandatory, self::GROUP_key2nd]),
-            $I->grabCookie(self::COOKIENAME, ['path' => self::PATH_root])
+            $this->cookieValueForGroups([Constants::GROUP_keyMandatory, Constants::GROUP_key2nd]),
+            $I->grabCookie(Constants::COOKIENAME, ['path' => Constants::PATH_root])
         );
     }
 
@@ -166,32 +114,23 @@ class PopupInteractionsCest
      */
     public function onScriptLoadedEventHandler(AcceptanceTester $I)
     {
-        $I->amOnPage(self::PATH_root);
+        $I->amOnPage(Constants::PATH_root);
         $I->setCookie(
-            self::COOKIENAME,
+            Constants::COOKIENAME,
             $this->cookieValueForGroups(
-                [self::GROUP_keyMandatory, self::GROUP_keyTestgroup]
+                [Constants::GROUP_keyMandatory, Constants::GROUP_keyTestgroup]
             )
         );
         $I->reloadPage();
 
         // test onScriptLoaded() (once as a callback and once when already loaded)
         foreach ([0, 1] as $iScript) {
-            $onScriptLoadedArgs = [self::TRACKINGOBJECT_inTestgroupWith2Scripts, $iScript];
+            $onScriptLoadedArgs = [Constants::TRACKINGOBJECT_inTestgroupWith2Scripts, $iScript];
             $I->executeJS(
-                self::JS_onScriptLoaded,
+                Constants::JS_onScriptLoaded,
                 $onScriptLoadedArgs
             );
             $I->waitForText($onScriptLoadedArgs[0] . ':' . $onScriptLoadedArgs[1] . ' loaded');
         }
-    }
-
-    /**
-     * @param array $groupKeys
-     * @return string
-     */
-    protected function cookieValueForGroups(array $groupKeys)
-    {
-        return implode(self::COOKIE_separator, $groupKeys);
     }
 }
---
+++ Tests/Acceptance/Support/Constants.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package dmind/cookieman.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Dmind\Cookieman\Tests\Acceptance\Support;
+
+class Constants
+{
+    public const PATH_root = '/';
+    public const PATH_imprint = '/imprint';
+
+    public const SELECTOR_modal = '#cookieman-modal';
+    public const SELECTOR_btnDataCookiemanShow = '[data-cookieman-show]';
+    public const SELECTOR_btnSaveNotSaveAll = '[data-cookieman-save]:not([data-cookieman-accept-all]):not([data-cookieman-accept-none])';
+    public const SELECTOR_btnSaveNone = '[data-cookieman-save][data-cookieman-accept-none]';
+    public const SELECTOR_btnSaveAll = '[data-cookieman-save][data-cookieman-accept-all]';
+    public const LOCATOR_settings = ['xpath' => '//*[self::button or self::a][contains(., "Settings")]'];
+
+    public const COOKIENAME = 'CookieConsent';
+    public const COOKIE_separator = '|';
+
+    public const JS_showCookieman = 'cookieman.show()';
+    public const JS_onScriptLoaded = "
+            cookieman.onScriptLoaded(
+                arguments[0],
+                arguments[1],
+                function (trackingObjectKey, scriptId) {
+                    document.body.append(arguments[0] + ':' + arguments[1] + ' loaded; ')
+                }
+            );
+        ";
+
+    public const GROUP_keyMandatory = 'mandatory';
+
+    public const GROUP_key2nd = 'marketing';
+    public const GROUP_title2nd = 'Marketing';
+    public const COOKIE_titleIn2ndGroup = '_gat';
+
+    public const GROUP_keyTestgroup = 'testgroup';
+    public const TRACKINGOBJECT_inTestgroupWith2Scripts = 'Crowdin';
+
+    public const WAITFOR_timeout = 5;
+}
