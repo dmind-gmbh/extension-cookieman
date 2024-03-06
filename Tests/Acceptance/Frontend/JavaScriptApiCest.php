6bb021e3da1e2806d6c35cdd83d36f712ae3bd50
Jonas Eberle <flightvision@googlemail.com>
2024-03-06 19:10:57 +0100

    chore(tests): refactor acceptance tests

---
+++ Tests/Acceptance/Frontend/JavaScriptApiCest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package dmind/cookieman.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Dmind\Cookieman\Tests\Acceptance\Frontend;
+
+use Codeception\Exception\ModuleException;
+use Dmind\Cookieman\Tests\Acceptance\Support\AcceptanceTester;
+use Dmind\Cookieman\Tests\Acceptance\Support\Constants;
+use Exception;
+
+/**
+ * Tests clicking through some frontend pages
+ */
+class JavaScriptApiCest
+{
+    /**
+     * @param AcceptanceTester $I
+     * @throws ModuleException
+     * @throws Exception
+     */
+    public function onScriptLoadedEventHandler(AcceptanceTester $I)
+    {
+        $I->amOnPage(Constants::PATH_root);
+        $I->setCookie(
+            Constants::COOKIENAME,
+            $this->cookieValueForGroups(
+                [Constants::GROUP_keyMandatory, Constants::GROUP_keyTestgroup]
+            )
+        );
+        $I->reloadPage();
+
+        // test onScriptLoaded() (once as a callback and once when already loaded)
+        foreach ([0, 1] as $iScript) {
+            $onScriptLoadedArgs = [Constants::TRACKINGOBJECT_inTestgroupWith2Scripts, $iScript];
+            $I->executeJS(
+                Constants::JS_onScriptLoaded,
+                $onScriptLoadedArgs
+            );
+            $I->waitForText($onScriptLoadedArgs[0] . ':' . $onScriptLoadedArgs[1] . ' loaded');
+        }
+    }
+
+    /**
+     * @param array $groupKeys
+     * @return string
+     */
+    protected function cookieValueForGroups(array $groupKeys)
+    {
+        return implode(Constants::COOKIE_separator, $groupKeys);
+    }
+}
--- Tests/Acceptance/Frontend/OpenPopupCest.php
+++ Tests/Acceptance/Frontend/OpenPopupCest.php
@@ -11,21 +11,8 @@ declare(strict_types=1);
 
 namespace Dmind\Cookieman\Tests\Acceptance\Frontend;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
-use Codeception\Util\Locator;
 use Dmind\Cookieman\Tests\Acceptance\Support\AcceptanceTester;
+use Dmind\Cookieman\Tests\Acceptance\Support\Constants;
 use Exception;
 
 /**
@@ -33,19 +20,29 @@ use Exception;
  */
 class OpenPopupCest
 {
-    public const PATH_imprint = '/imprint';
-    public const SELECTOR_modal = '#cookieman-modal';
-    public const SELECTOR_btnDataCookiemanShow = '[data-cookieman-show]';
-
     /**
      * @param AcceptanceTester $I
      * @throws Exception
      */
     public function openViaClickOnDataCookiemanShowElement(AcceptanceTester $I)
     {
-        $I->amOnPage(self::PATH_imprint);
+        $I->amOnPage(Constants::PATH_imprint);
         $I->waitForJS('return typeof cookieman === "object"', 10);
-        $I->clickWithLeftButton(['css' => self::SELECTOR_btnDataCookiemanShow]);
-        $I->waitForElementVisible(self::SELECTOR_modal);
+        $I->clickWithLeftButton(['css' => Constants::SELECTOR_btnDataCookiemanShow]);
+        $I->waitForElementVisible(Constants::SELECTOR_modal);
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @throws Exception
+     */
+    public function notShownOnImprint(AcceptanceTester $I)
+    {
+        $I->amOnPage(Constants::PATH_imprint);
+        $I->waitForJS('return typeof cookieman === "object"', 10);
+        $I->wait(Constants::WAITFOR_timeout);
+        $I->dontSeeElement(Constants::SELECTOR_modal);
+        $I->executeJS(Constants::JS_showCookieman);
+        $I->waitForElementVisible(Constants::SELECTOR_modal, Constants::WAITFOR_timeout);
     }
 }
