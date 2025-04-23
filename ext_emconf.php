9058d63d4b3739c1a95f927b8f32e50f89f60c25
Jonas Eberle <flightvision@googlemail.com>
2025-04-23 16:27:28 +0200

    task: prepare for TYPO3 v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.9.99',
+            'typo3' => '14.0.0-14.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
