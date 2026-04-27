0978b20707e18b8ea776a7e1485b02cc8f2523a1
Jonas Eberle <flightvision@googlemail.com>
2026-04-27 12:51:23 +0200

    chore(ci): lint ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the package dmind/cookieman.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Cookieman',
     'description' => 'Provides a user consent popup. It asks for approval to include tracking objects (cookies, images or any HTML) and includes the objects when consented. It also removes cookies after the consent has been revoked.',
