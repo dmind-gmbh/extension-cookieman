b0e2dd43a63e70ef68aa50c9ee956a1962cf7c15
Jonas Eberle <flightvision@googlemail.com>
2026-04-27 12:19:20 +0200

    chore(ci): add typo3-rector and add to ci baseline

---
+++ rector.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PostRector\Rector\NameImportingPostRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\ValueObject\PhpVersion;
+use Ssch\TYPO3Rector\CodeQuality\General\ConvertImplicitVariablesToExplicitGlobalsRector;
+use Ssch\TYPO3Rector\CodeQuality\General\ExtEmConfRector;
+use Ssch\TYPO3Rector\Configuration\Typo3Option;
+use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
+use Ssch\TYPO3Rector\Set\Typo3SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/Build',
+        __DIR__ . '/Classes',
+        __DIR__ . '/Configuration',
+        __DIR__ . '/Tests',
+        __DIR__ . '/config',
+        __DIR__ . '/ext_emconf.php',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withPhpVersion(PhpVersion::PHP_82)
+    ->withSets([
+        // Rector rules
+        SetList::CODE_QUALITY,
+        LevelSetList::UP_TO_PHP_82,
+
+        Typo3SetList::CODE_QUALITY,
+        Typo3SetList::GENERAL,
+        Typo3LevelSetList::UP_TO_TYPO3_14,
+        // To migrate to Doctrine Dbal 4, uncomment the following line
+        //\Rector\Doctrine\Set\DoctrineSetList::DOCTRINE_DBAL_40,
+    ])
+    // To have a better analysis from PHPStan, we teach it here some more things
+    ->withPHPStanConfigs([Typo3Option::PHPSTAN_FOR_RECTOR_PATH])
+    ->withImportNames(true, true, false, true)
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
+    ->withConfiguredRule(ExtEmConfRector::class, [
+        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.2.0-8.5.99',
+        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '14.0.0-14.9.99',
+        ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
+    ])
+    ->withSkip([
+        // @see https://github.com/sabbelasichon/typo3-rector/issues/2536
+        __DIR__ . '/**/Configuration/ExtensionBuilder/*',
+        __DIR__ . '/config/system/settings.php',
+        NameImportingPostRector::class => [
+            'ClassAliasMap.php',
+        ],
+    ]);
